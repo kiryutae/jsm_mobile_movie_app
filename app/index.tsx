aefc8138e9340bc5696130e3fa935991f342c252
Alex <cihuizou@gmail.com>
2026-02-14 17:07:07 +0800

    配置主題色

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl font-bold">Welcome to my Movie App</Text>
+      <Text className="text-2xl font-bold text-primary">
+        Welcome to my Movie App
+      </Text>
     </View>
   );
 }
