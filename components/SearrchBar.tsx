8b5def0d06215f70c1b41e75cdc57096e46c45d7
Alex <cihuizou@gmail.com>
2026-02-14 23:45:26 +0800

    搜尋組件及搜尋功能

--- components/SearrchBar.tsx
+++ components/SearrchBar.tsx
@@ -2,11 +2,13 @@ import { icons } from "@/constants/icons";
 import { Image, TextInput, View } from "react-native";
 
 interface Props {
-  onPress: () => void;
+  onPress?: () => void;
   placeholder: string;
+  value?: string;
+  onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({ onPress, placeholder }: Props) => {
+const SearchBar = ({ onPress, placeholder, value, onChangeText }: Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-1">
       <Image
@@ -18,8 +20,8 @@ const SearchBar = ({ onPress, placeholder }: Props) => {
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor="#A8B5DB"
         className="flex-1 text-white ml-2"
       />
