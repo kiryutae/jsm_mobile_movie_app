26f5ebb1903188e543a3bbdda0b5a9174cda1b81
Alex <cihuizou@gmail.com>
2026-02-14 19:43:31 +0800

    首頁樣式及搜尋組件

---
+++ components/SearrchBar.tsx
@@ -0,0 +1,30 @@
+import { icons } from "@/constants/icons";
+import { Image, TextInput, View } from "react-native";
+
+interface Props {
+  onPress: () => void;
+  placeholder: string;
+}
+
+const SearchBar = ({ onPress, placeholder }: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-1">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#AB8BFF"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#A8B5DB"
+        className="flex-1 text-white ml-2"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
