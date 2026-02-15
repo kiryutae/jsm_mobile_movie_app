aa08f4761729cc2a436d5e4bb16266ad75e3f70e
Alex <cihuizou@gmail.com>
2026-02-15 11:35:19 +0800

    用戶頁及書籤頁初始化

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,8 +1,13 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { Image, Text, View } from "react-native";
 
 const Saved = () => {
   return (
-    <View>
+    <View className="flex-1 bg-primary px-10">
+      <View className="flex-1 items-center justify-center flex-col gap-5">
+        <Image source={icons.save} className="size-20" tintColor="#fff" />
+        <Text className="text-gray-500 text-base">Saved</Text>
+      </View>
       <Text>Saved</Text>
     </View>
   );
